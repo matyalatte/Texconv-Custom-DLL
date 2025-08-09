4b057b946b8b2e8f41b63b7c210a179cee3029f9
matyalatte <matyalatte@gmail.com>
2025-08-09 16:29:06 +0900

    remove unused functions from tool_util.h

--- TexconvDLL/tool_util.h
+++ TexconvDLL/tool_util.h
@@ -10,10 +10,6 @@ extern "C"{
 }
 #include <filesystem>
 
-//#define S_OK 0
-//#define E_FAIL 0x80004005
-//#define FAILED(hr) hr != S_OK
-
 // Todo: Use secure function
 #define swscanf_s swscanf
 #define swprintf_s swprintf
@@ -29,105 +25,7 @@ extern "C"{
 // Todo: define _wcslwr_s for unix
 // (for USE_NAME_CONFIG)
 
-// wmakepath_s for unix
-template <size_t size>
-void _wmakepath_s(
-   wchar_t (&path)[size],
-   const wchar_t *drv,
-   const wchar_t *dir,
-   const wchar_t *fname,
-   const wchar_t *ext
-) {
-    wchar_t* end = &path[0] + size;    /* end of path buffer */
-    wchar_t* s = &path[0];      /* copy pointer */
-    if (drv) {
-        for(;*drv && *drv != '\0' && s < end;)
-            *s++ = *drv++;
-    }
-    if (dir) {
-        for(;*dir && *dir!= '\0' && s < end;)
-            *s++ = *dir++;
-    }
-    if (s > &path[0] && s < end && (*(s - 1) != std::filesystem::path::preferred_separator)) {
-        *s++ = std::filesystem::path::preferred_separator;
-    }
-    if (fname) {
-        for(; *fname && *fname!= '\0' && s < end;)
-            *s++ = *fname++;
-    }
-    if (s < end) {
-        *s++ = '.';
-    }
-    if (ext) {
-        for(; *ext && *ext!= '\0' && s < end;)
-            *s++ = *ext++;
-    }
-    if (s < end) {
-        *s = '\0';
-    }
-}
-
-// wsplitpath_s for unix
-static void _wsplitpath_s(const WCHAR* path, WCHAR* drv, int drvnum, WCHAR* dir, int dirnum, WCHAR* name, int namenum, WCHAR* ext, int extnum)
-{
-    const WCHAR* end; /* end of processed string */
-    const WCHAR* p;      /* search pointer */
-    const WCHAR* s;      /* copy pointer */
-
-    /* extract drive name */
-    if (path[0] && path[1]==':') {
-            if (drv) {
-                    *drv++ = *path++;
-                    *drv++ = *path++;
-                    *drv = '\0';
-            }
-    } else if (drv)
-            *drv = '\0';
-
-    /* search for end of string or stream separator */
-    for(end=path; *end && *end!=':'; )
-            end++;
-
-
-    /* search for begin of file extension */
-    for(p=end; p>path && *--p!='\\' && *p!='/'; )
-            if (*p == '.') {
-                    end = p;
-                    break;
-            }
-
-    if (ext)
-            for(s=end; (*ext=*s++); )
-                    ext++;
-
-    /* search for end of directory name */
-    for(p=end; p>path; )
-            if (*--p=='\\' || *p=='/') {
-                    p++;
-                    break;
-            }
-
-    if (name) {
-            for(s=p; s<end; )
-                    *name++ = *s++;
-
-            *name = '\0';
-    }
-
-    if (dir) {
-            for(s=path; s<p; )
-                    *dir++ = *s++;
-
-            *dir = '\0';
-    }
-}
-#endif
-
-#ifdef _WIN32
-#define ErrorIsMissingPath(hr) hr == -2147024893;
-#else
-#define ErrorIsMissingPath(hr) hr == -2147467259;
-#endif
+#endif  // _WIN32
 
 #define RaiseError(...) \
     fwprintf(stderr, __VA_ARGS__); \

aa9e72b9b1bd7e2a5bae14b77f24e31d53ccb175
matyalatte <matyalatte@gmail.com>
2025-08-09 16:23:57 +0900

    download sal.h with cmake

--- shell_scripts/get_sal.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Download sal.h for DirectXMath
-
-cd $(dirname "$0")/../unix_external
-mkdir sal
-cd sal
-wget https://raw.githubusercontent.com/dotnet/corert/master/src/Native/inc/unix/sal.h
